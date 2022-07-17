7a95931225faabe245e4763c9292478d2d41dd32
Minju <minju1009@gmail.com>
2022-07-17 19:30:03 +0900

    Fix : package.json conflict 해결

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Detail from 'pages/Detail/Detail';
-<<<<<<< HEAD
 import Main from 'pages/Main/Main';
-=======
 import Login from 'pages/Login/Login';
 import Join from 'pages/Join/Join';
 import Main from 'pages/Main';
->>>>>>> 13dab71 (Add : filter 및 pagination 기능 추가 & 컴포넌트화)
 import Creation from 'pages/Creation/Creation';
 
 const Router = () => {
