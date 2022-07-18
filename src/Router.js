ba764561bab985bb13e29ca8ccda9f78c6eea48d
Geuni <dlrmsgnl620@naver.com>
2022-07-18 18:52:12 +0900

    Modify: Router 에러 수정

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,7 @@
 import * as React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Detail from 'pages/Detail/Detail';
-import Main from 'pages/Main/Main';
-import Login from 'pages/Login/Login';
-import Join from 'pages/Join/Join';
-import Main from 'pages/Main';
+import Main from 'pages/Main/index';
 import Creation from 'pages/Creation/Creation';
 import Edit from 'pages/Edit/Edit';
 
